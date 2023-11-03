e0f6589c3977b7e6efd9efc82db8bf8ae46dbc3f
howard <idhowardgj94@gmail.com>
2023-11-04 02:26:06 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   mode: "jit",
   content: [
-    "./frontend/resources/**/*.html",
-    "./frontend/resources/public/js/main.js"
+      "./frontend/resources/**/*.html",
+      process.env.NODE_ENV == "production" ? "./frontend/resource/public/js/main.js" :"./frontend/resources/public/js/cljs-runtime/*.js"
   ],
   theme: {
     extend: {}
