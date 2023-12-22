ca42a020245bacfe13621f2234dc7ba17a23578e
howard chang <idhowardgj94@gmail.com>
2023-12-23 00:35:46 +0800

    feat: export-fn support var quote (ref)

--- components/tools/src/com/howard/uchat/backend/tools/macro.clj
+++ components/tools/src/com/howard/uchat/backend/tools/macro.clj
@@ -4,8 +4,25 @@
 (defmacro export-fn
   "export a function by given name and a function ref"
   [sym f]
-  (let [{:keys [doc arglists]} (meta (resolve f))]
-    `(def  
+  (let [{:keys [doc arglists]} (if (coll? f)
+                                 (meta (resolve (symbol (eval f))))
+                                 (meta (resolve f)))]
+    ;; note: arglists type will like ([a])
+    ;; so need to change to quote @arglists (quote ([a]))
+    `(def
        ~(with-meta sym {:doc doc
-                        :arglists `'~arglists
-                        }) ~f)))
+                        :arglists `(quote ~arglists)}) ~f)))
+
+(defmacro dbfn
+  {:clj-kondo/lint-as 'clojure.core/defn}
+  [name & args]
+  (let [doc (if (string? (first args)) (first args) nil)
+        params (if (nil? doc) (first args) (second args))
+        second-params (if (<= (count params) 1) [] (into [] (drop 1 params)))
+        body (if (nil? doc) (drop 1 args) (drop 2 args))]
+    `(defn ~name
+         ~doc
+         (~params
+         ~@body)
+         (~second-params
+          (apply ~name (get-pool) ~second-params)))))
